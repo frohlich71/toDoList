99bce9405ef1e72efb61d3f305083f911a603227
Alexandre Silva <alexandresilva@Alexandres-MacBook-Air.local>
2022-07-14 10:58:24 -0300

    terminando input task e definindo body color

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,15 @@ import { Input } from "./components/Input"
 
 export function App () {
   return (
-    <div>
-      <Header/>
-      <Input/>
+    <div className="relative">
+        <Header/>
+      <div className="justify-center flex mt-[-2rem] ml-14 relative">
+        <Input/>
+        <main>
+        
+        </main>
+      </div>
+
     </div>
   )
 }
--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,10 +1,8 @@
 export function Input () {
   return (
-    <div className="justify-center flex">
       <form>
         <input type="text" id="task" name="task" placeholder="Adicione uma nova tarefa" className="bg-[#262626] shadow-md shadow-black w-110 h-14 p-3 rounded-xl outline outline-2 outline-[#0D0D0D] text-white  "/>
-        <button>Criar</button>
+        <button type="button" className="bg-[#1E6F9F] w-16 h-14 rounded-xl ml-3 text-white shadow-md shadow-black outline outline-1 outline-[#0D0D0D]">Criar</button>
       </form>
-    </div>
   )
 }
