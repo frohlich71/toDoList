8fd974a93d51e0b620117690a9b9a31d227ae1e5
Frohlich71 <victorfrohlich520@gmail.com>
2022-07-16 21:01:15 -0300

    layout lista vazia

---
+++ src/components/Task.tsx
@@ -0,0 +1,5 @@
+export function Task () {
+  return (
+    <h1>Hello world</h1>
+  )
+}
--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,3 +1,6 @@
+import { Task } from "./Task";
+import clipBoard from "../assets/Clipboard.svg"
+
 export function TaskList () {
   return (
     <article>
@@ -15,6 +18,14 @@ export function TaskList () {
           </span>
         </div>
       </header>
+
+      <div className="shadow-4zl relative rounded-xl mt-6">
+        <div className="pt-14 pl-52">
+        <img src={clipBoard} alt="Imagem de um bloco de notas" className="ml-[7.5rem] mb-5" />
+        <strong className="text-[#808080]">Você ainda não tem tarefas cadastradas</strong>
+        <p className="text-[#808080] text-sm pl-7 pt-1">Crie tarefas e organize seus itens a fazer</p>
+        </div>
+      </div>
     </article>
   )
 }
