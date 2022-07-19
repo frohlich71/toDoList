b2155bca822c6c01508ae8aee9b23df313619bb1
Victor Frohlich <victorfrohlich520@gmail.com>
2022-07-19 19:08:00 -0300

    terminando html e css

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,22 @@
+import { EmptyList } from "./components/EmptyList"
 import { Header } from "./components/Header"
 import { Input } from "./components/Input"
-import { TaskList } from "./components/TaskList"
+import { Task } from "./components/Task"
+
+const tasks = [
+  {
+    id: 1,
+    content: 'Fazer 3 exercicios',
+    status: false
+
+  },
+  {
+    id: 2,
+    content: 'Estudar 3 horas',
+    status: true
+  }
+]
+
 
 export function App () {
   return (
@@ -11,9 +27,24 @@ export function App () {
       </div>
 
       <main className="justify-center flex relative">
-        <TaskList />
+      <article>
+        <header className="inline-flex mt-24 mb-10">
+          <div className="mr-[28rem] text-[#4EA8DE] font-bold">
+            Tarefas criadas
+            <span className="bg-[#333333] text-white rounded-full p-1 pl-3 pr-3 ml-2">
+              0
+            </span>
+          </div>
+          <div className="text-[#8284FA] justify-end flex font-bold">
+            Concluidas
+            <span className="bg-[#333333] text-white rounded-full  pl-3 pr-3 ml-2">
+              0
+            </span>
+          </div>
+        </header>
+        <Task />
+      </article>
       </main>
-
     </div>
   )
 }
--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,5 +1,13 @@
+import {Trash} from 'phosphor-react'
+
 export function Task () {
   return (
-    <h1>Hello world</h1>
+    <div className='pt-6 flex w-[46rem] h-24 outline outline-[#262626] shadow-md shadow-black rounded-lg pl-6 text-ellipsis overflow-scroll'>
+      <input type="checkbox" className="rounded-full bg-transparent w-6 h-6 absolute shadow-lg" />
+      <div className='w-[37rem] content-center'>
+      <div className="text-white pl-20 text-left font-normal shadow">Lorem ipsum dolor sit amet consectetur, adipisicing elit. </div>
+      </div>
+      <Trash  size='22' className='text-zinc-600 absolute ml-[40rem] shadow'/>
+    </div>
   )
 }
